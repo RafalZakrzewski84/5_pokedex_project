a0ec68991c448cca7834e42a102c9efeaf2fb6d8
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-01 10:57:58 +0200

    5_34_Creating Pokecard Component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
-			<Pokecard />
+			<Pokecard id={4} name="Charmander" type="fire" base_experience={62} />
 		</div>
 	);
 }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,11 +1,19 @@
 /** @format */
 
 import React from 'react';
+import './Pokecard.css';
+
+const POKE_API =
+	'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 function Pokecard(props) {
+	let imgSrc = `${POKE_API}${props.id}.png`;
 	return (
 		<div className="Pokecard">
-			<h1>props.name</h1>
+			<h1>{props.name}</h1>
+			<img src={imgSrc} alt={props.name} />
+			<div>Type: {props.type}</div>
+			<div>EXP: {props.base_experience}</div>
 		</div>
 	);
 }
